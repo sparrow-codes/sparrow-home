6e51b3f19a8eb320ef0b02f1f745a13d6f2ab561
Piotr Wróbel <wrobel.j.piotr@gmail.com>
2024-10-02 14:21:30 +0200

    feature/adding database connection

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,11 +1,32 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { ConfigKey } from './enums/config-key';
 import { CloudModule } from './modules/cloud/cloud.module';
 import { DeviceModule } from './modules/device/device.module';
 
 @Module({
-  imports: [CloudModule, HttpModule, DeviceModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    CloudModule,
+    HttpModule,
+    DeviceModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>(ConfigKey.DB_HOST),
+        port: +configService?.get<number>(ConfigKey.DB_PORT),
+        username: configService?.get<string>(ConfigKey.DB_USER_NAME),
+        password: configService?.get<string>(ConfigKey.DB_PASSWORD),
+        schema: configService?.get<string>(ConfigKey.DB_SCHEMA),
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+    }),
+  ],
 })
 export class AppModule {}
