c213046b23cc3b5fdd03e5a0b212cefcfbfb27e5
Piotr <44137367+PiotrWrobel@users.noreply.github.com>
2024-10-02 14:30:12 +0200

    feature/adding database connection (#3)

--- apps/server/src/app/enums/config-key.ts
+++ apps/server/src/app/enums/config-key.ts
@@ -1,4 +1,10 @@
 export enum ConfigKey {
   PANASONIC_CLOUD_LOGIN = 'panasonicCloudLogin',
   PANASONIC_CLOUD_PASSWORD = 'panasonicCloudPassword',
+
+  DB_HOST = 'dbHost',
+  DB_PORT = 'dbPort',
+  DB_USER_NAME = 'dbUserName',
+  DB_PASSWORD = 'dbPassword',
+  DB_SCHEMA = 'dbSchema',
 }
