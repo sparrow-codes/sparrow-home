1ddbe67696195154ba34243328b484ad5a6ad1c4
Piotr Wróbel <wrobel.j.piotr@gmail.com>
2025-04-01 20:41:50 +0200

    feature/adding main page navigation on app visibility

--- libs/fe/core/src/lib/services/index.ts
+++ libs/fe/core/src/lib/services/index.ts
@@ -2,4 +2,5 @@ export * from './data-facade.service';
 export * from './loader.service';
 export * from './mobile-authentication.service';
 export * from './mobile-push-notification.service';
+export * from './visibility.service';
 export * from './web-authentication.service';
