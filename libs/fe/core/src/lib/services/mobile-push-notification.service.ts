ce8fbb4e31f5e6245a9a723230ea6c0d0af47934
Piotr Wróbel <wrobel.j.piotr@gmail.com>
2025-04-05 21:38:45 +0200

    fix/fix for sending push details

--- libs/fe/core/src/lib/services/mobile-push-notification.service.ts
+++ libs/fe/core/src/lib/services/mobile-push-notification.service.ts
@@ -1,7 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal, WritableSignal } from '@angular/core';
 import { SwPush } from '@angular/service-worker';
 import { PushNotificationApiService } from '@sparrow-home/api';
-import { first } from 'rxjs';
+import { filter, first, switchMap } from 'rxjs';
 
 import { DataFacadeService } from './data-facade.service';
 
@@ -12,19 +13,33 @@ export class MobilePushNotificationService {
   private readonly _swPush: SwPush = inject(SwPush);
   private readonly _rootDataService: DataFacadeService = inject(DataFacadeService);
   private readonly _pushNotificationApiService: PushNotificationApiService = inject(PushNotificationApiService);
+  private readonly _httpService: HttpClient = inject(HttpClient);
   private readonly _subscribed: WritableSignal<boolean> = signal(false);
 
-  public subscribeToNotifications(): void {
+  public constructor() {
     const webPushPublicKey: string | undefined = this._rootDataService.applicationConfig()?.webPushPublicKey;
 
     if (webPushPublicKey && !this._subscribed()) {
-      this._swPush.requestSubscription({ serverPublicKey: webPushPublicKey }).then((pushNotification) => {
-        this._pushNotificationApiService
-          .addSubscription({ body: pushNotification })
-          .pipe(first())
-          .subscribe(() => this._subscribed.set(true));
-      });
+      this._swPush.requestSubscription({ serverPublicKey: webPushPublicKey });
     }
+  }
+
+  public subscribeToNotifications(): void {
+    this._swPush.subscription
+      .pipe(
+        filter((subscription) => !!subscription),
+        first(),
+        switchMap((subscription) =>
+          this._httpService.post(
+            `${this._pushNotificationApiService.rootUrl}/api/push-notification/add-subscription`,
+            subscription,
+            {
+              headers: { 'ngsw-bypass': 'true' },
+            }
+          )
+        )
+      )
+      .subscribe();
   }
 
   public subscribeMessage(): void {
