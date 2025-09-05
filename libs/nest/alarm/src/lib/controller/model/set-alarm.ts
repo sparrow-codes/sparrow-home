e2e4ef4e75aa6d8e4c877d8ee32388784f8d4b4d
Piotr <44137367+PiotrWrobel@users.noreply.github.com>
2025-09-05 22:07:24 +0200

    fix - fix for turning alarm off (#102)

--- libs/nest/alarm/src/lib/controller/model/set-alarm.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
-
-export class SetAlarm {
-  @IsNotEmpty({ message: 'Field is required' })
-  @ApiProperty()
-  public isOn!: boolean;
-}
