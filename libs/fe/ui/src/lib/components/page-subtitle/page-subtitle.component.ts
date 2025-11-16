51fdd886703c8ab6a7fcc276a785a161bfd6f7de
Piotr <44137367+PiotrWrobel@users.noreply.github.com>
2025-11-16 20:26:33 +0100

    chore/code clean up (#124)

--- libs/fe/ui/src/lib/components/page-subtitle/page-subtitle.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'sp-page-subtitle',
-  imports: [],
-  templateUrl: './page-subtitle.component.html',
-})
-export class PageSubtitleComponent {}
