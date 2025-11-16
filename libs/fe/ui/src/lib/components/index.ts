0ca83e52b470bf690cb10e623be274adeff3d804
Piotr <44137367+PiotrWrobel@users.noreply.github.com>
2025-11-16 20:26:33 +0100

    chore/code clean up (#124)

--- libs/fe/ui/src/lib/components/index.ts
+++ libs/fe/ui/src/lib/components/index.ts
@@ -2,8 +2,6 @@ export * from './battery-status/battery-status.component';
 export * from './confirmation-dialog/confirmation-dialog.component';
 export * from './confirmation-dialog/model/confirmation-dialog-data';
 export * from './device-action/device-action.component';
-export * from './face-id-icon/face-id-icon.component';
 export * from './frame/mobile-frame.component';
 export * from './page-not-found/page-not-found.component';
-export * from './page-subtitle/page-subtitle.component';
 export * from './page-title/page-title.component';
