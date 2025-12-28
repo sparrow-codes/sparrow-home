343b12ead6dcc133e7b6e1174753d25a80b2578e
Piotr Wróbel <wrobel.j.piotr@gmail.com>
2025-12-28 17:12:10 +0100

    fix - fixes after package updates

--- apps/sparrow-home-mobile/src/theme/noir.ts
+++ apps/sparrow-home-mobile/src/theme/noir.ts
@@ -20,7 +20,7 @@ export const Noir = definePreset(Aura, {
       light: {
         primary: {
           color: '{zinc.950}',
-          inverseColor: '#ffffff',
+          contrastColor: '#ffffff',
           hoverColor: '{zinc.900}',
           activeColor: '{zinc.800}',
         },
@@ -34,7 +34,7 @@ export const Noir = definePreset(Aura, {
       dark: {
         primary: {
           color: '{zinc.50}',
-          inverseColor: '{zinc.950}',
+          contrastColor: '{zinc.950}',
           hoverColor: '{zinc.100}',
           activeColor: '{zinc.200}',
         },
