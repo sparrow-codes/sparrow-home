37eac535cff8de18697c785b0dcf5c6318a2fd91
Piotr <44137367+PiotrWrobel@users.noreply.github.com>
2026-01-14 16:45:52 +0100

    fix for selecting device type order (#163)

--- libs/fe/ui/src/lib/dictionary/device-type-dictionary.ts
+++ libs/fe/ui/src/lib/dictionary/device-type-dictionary.ts
@@ -1,10 +1,10 @@
 import { DeviceType } from '@sparrow-home/utils';
 
 export const deviceTypeDictionary: Map<DeviceType, string> = new Map([
+  [DeviceType.OTHER, 'ui.device_types.other'],
   [DeviceType.POWER_PLUG, 'ui.device_types.power_plug'],
   [DeviceType.TEMPERATURE_SENSOR, 'ui.device_types.temperature_sensor'],
   [DeviceType.OPEN_DOOR_SENSOR, 'ui.device_types.open_door_sensor'],
   [DeviceType.SIREN, 'ui.device_types.siren'],
   [DeviceType.PILOT, 'ui.device_types.pilot'],
-  [DeviceType.OTHER, 'ui.device_types.other'],
 ]);
