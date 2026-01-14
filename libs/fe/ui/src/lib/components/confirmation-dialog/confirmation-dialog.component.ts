908ed5dfd8a07f5c330dd2268e5e6b34e0808773
Piotr Wróbel <wrobel.j.piotr@gmail.com>
2026-01-14 19:47:17 +0100

    fixes after review

--- libs/fe/ui/src/lib/components/confirmation-dialog/confirmation-dialog.component.ts
+++ libs/fe/ui/src/lib/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { TranslatePipe } from '@ngx-translate/core';
 import { ButtonDirective } from 'primeng/button';
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
@@ -12,8 +13,11 @@ import { ConfirmationDialogData } from './model/confirmation-dialog-data';
   templateUrl: './confirmation-dialog.component.html',
 })
 export class ConfirmationDialogComponent {
+  private readonly _domSanitizer: DomSanitizer = inject(DomSanitizer);
+
   protected readonly data: ConfirmationDialogData = inject(DynamicDialogConfig).data;
   protected readonly dialogRef: DynamicDialogRef = inject(DynamicDialogRef);
+  protected readonly content: SafeHtml = this._domSanitizer.bypassSecurityTrustHtml(this.data.content);
 
   protected onConfirm(): void {
     this.dialogRef.close(true);
