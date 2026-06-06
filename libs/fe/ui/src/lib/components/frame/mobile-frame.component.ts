7e8cb433d80521db2f56a4121ce478aacd96a78e
Piotr Wróbel <wrobel.j.piotr@gmail.com>
2026-06-06 17:02:53 +0200

    fixes after review

--- libs/fe/ui/src/lib/components/frame/mobile-frame.component.ts
+++ libs/fe/ui/src/lib/components/frame/mobile-frame.component.ts
@@ -1,9 +1,7 @@
-import { Component, inject, Signal } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NgIcon } from '@ng-icons/core';
 import { TranslatePipe } from '@ngx-translate/core';
-import { filter, map } from 'rxjs';
 
 import { NavigationItem } from '../../models/navigation-item';
 
@@ -13,17 +11,7 @@ import { NavigationItem } from '../../models/navigation-item';
   templateUrl: './mobile-frame.component.html',
 })
 export class MobileFrameComponent {
-  private readonly _router: Router = inject(Router);
   private readonly _activeRoute: ActivatedRoute = inject(ActivatedRoute);
 
   protected readonly navigationItems: NavigationItem[] = this._activeRoute.snapshot.data['navigationItems'];
-
-  protected currentUrl: Signal<string | undefined> = toSignal(
-    this._router.events.pipe(
-      filter((event) => event instanceof NavigationEnd),
-      map(() => {
-        return this._router.url;
-      })
-    )
-  );
 }
