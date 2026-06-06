8e231848674ef6d3bbd88707211565bcae1fe22b
Piotr <44137367+PiotrWrobel@users.noreply.github.com>
2026-06-06 17:12:02 +0200

    add devices navigation item and update icons in mobile frame (#203)
    * add devices navigation item and update icons in mobile frame
    
    * fixes after review

--- libs/fe/ui/src/lib/components/frame/mobile-frame.component.ts
+++ libs/fe/ui/src/lib/components/frame/mobile-frame.component.ts
@@ -1,28 +1,17 @@
-import { Component, inject, Signal } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NgIcon } from '@ng-icons/core';
 import { TranslatePipe } from '@ngx-translate/core';
-import { filter, map } from 'rxjs';
 
 import { NavigationItem } from '../../models/navigation-item';
 
 @Component({
   selector: 'sp-mobile-frame',
-  imports: [RouterOutlet, RouterLink, TranslatePipe],
+  imports: [RouterOutlet, RouterLink, TranslatePipe, NgIcon, RouterLinkActive],
   templateUrl: './mobile-frame.component.html',
 })
 export class MobileFrameComponent {
-  private readonly _router: Router = inject(Router);
   private readonly _activeRoute: ActivatedRoute = inject(ActivatedRoute);
 
   protected readonly navigationItems: NavigationItem[] = this._activeRoute.snapshot.data['navigationItems'];
-
-  protected currentUrl: Signal<string | undefined> = toSignal(
-    this._router.events.pipe(
-      filter((event) => event instanceof NavigationEnd),
-      map(() => {
-        return this._router.url;
-      })
-    )
-  );
 }
